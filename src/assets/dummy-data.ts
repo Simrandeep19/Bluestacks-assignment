f346f817096170100fb4e91845f4e38aaf6878f9
Simrandeep Singh <singhsimrandeep19@gmail.com>
2019-11-28 19:02:40 +0530

    Filling Dummy Data

--- src/assets/dummy-data.ts
+++ src/assets/dummy-data.ts
@@ -12,33 +12,73 @@ export const dummy ={
     },
     {
       "id":2,
-      "name": "Super Jewels Quest",
-      "region": "CA, FR",
+      "name": "PUBG Mobile",
+      "region": "India",
       "createdOn": 1559806715124,
-      "price": "Price info of Super Jewels Quest",
-      "csv": "Some CSV link for Super Jewels Quest",
-      "report": "Some report link for Super Jewels Ques",
-      "image_url":"./assets/Dashboard/Row Copy 2-Row/Thumb/Bitmap.png"
+      "price": "View Prcing",
+      "csv": "Need to check how to implement this functionality",
+      "report": "Need to check how to implement this functionality",
+      "image_url":"./assets/Dashboard/Row Copy 5-Row/Thumb/Bitmap.png"
     },
     {
       "id":3,
-      "name": "Mole Slayer",
-      "region": "FR",
+      "name": "Flow Free",
+      "region": "US",
       "createdOn": 1559806711124,
-      "price": "Price info of Mole Slayer",
-      "csv": "Some CSV link for Mole Slayer",
-      "report": "Some report link for Mole Slayer",
+      "price": "View Prcing",
+      "csv": "Need to check how to implement this functionality",
+      "report": "Need to check how to implement this functionality",
+      "image_url":"./assets/Dashboard/Row Copy 2-Row/Thumb/Bitmap.png"
+    },
+    {
+      "id":4,
+      "name": "Garena Free Fire...",
+      "region": "US",
+      "createdOn": 1559806680124,
+      "price": "View Pricing",
+      "csv": "Need to check how to implement this functionality",
+      "report": "Need to check how to implement this functionality",
       "image_url":"./assets/Dashboard/Row Copy 3-Row/Thumb/Bitmap.png"
     },
     {
       "id":4,
-      "name": "Mancala Mix",
-      "region": "JP",
+      "name": "Garena Free Fire...",
+      "region": "US",
       "createdOn": 1559806680124,
-      "price": "Price info of Mancala Mix",
-      "csv": "Some CSV link for Mancala Mix",
-      "report": "Some report link for Mancala Mix",
-      "image_url":"./assets/Dashboard/Row Copy 4-Row/Thumb/Bitmap.png"
+      "price": "View Pricing",
+      "csv": "Need to check how to implement this functionality",
+      "report": "Need to check how to implement this functionality",
+      "image_url":"./assets/Dashboard/Row Copy 3-Row/Thumb/Bitmap.png"
+    },
+    {
+      "id":4,
+      "name": "Garena Free Fire...",
+      "region": "US",
+      "createdOn": 1559806680124,
+      "price": "View Pricing",
+      "csv": "Need to check how to implement this functionality",
+      "report": "Need to check how to implement this functionality",
+      "image_url":"./assets/Dashboard/Row Copy 3-Row/Thumb/Bitmap.png"
+    },
+    {
+      "id":4,
+      "name": "Garena Free Fire...",
+      "region": "US",
+      "createdOn": 1559806680124,
+      "price": "View Pricing",
+      "csv": "Need to check how to implement this functionality",
+      "report": "Need to check how to implement this functionality",
+      "image_url":"./assets/Dashboard/Row Copy 3-Row/Thumb/Bitmap.png"
+    },
+    {
+      "id":4,
+      "name": "Garena Free Fire...",
+      "region": "US",
+      "createdOn": 1559806680124,
+      "price": "View Pricing",
+      "csv": "Need to check how to implement this functionality",
+      "report": "Need to check how to implement this functionality",
+      "image_url":"./assets/Dashboard/Row Copy 3-Row/Thumb/Bitmap.png"
     }
   ]
 }
